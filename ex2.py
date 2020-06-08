31d69107d007733319d4fee3b73a888559f66179
DARSHAN P B <darshanpb111@gmail.com>
2020-06-08 15:23:32 +0530

    ex2 comma code function

---
+++ ex2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+''' 
+Comma Code
+Say you have a list value like this:
+spam = ['apples', 'bananas', 'tofu', 'cats']
+Write a function that takes a list value as an argument and returns
+a string with all the items separated by a comma and a space, with and
+inserted before the last item. For example, passing the previous spam list to
+the function would return 'apples, bananas, tofu, and cats'. But your function
+should be able to work with any list value passed to it. '''
+def comma_code(parameter_list):
+    string = str(parameter_list[0])
+    for i in range(1,len(parameter_list)):
+        string += (", "+parameter_list[i])
+    return string
+if __name__ == "__main__":
+    spam = ['apples', 'bananas', 'tofu', 'cats']
+    out = comma_code(spam)
+    print(out)
