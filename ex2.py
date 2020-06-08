cd63b51de906fd9d58875bbe9a2af57fe6f2af8d
DARSHAN P B <darshanpb111@gmail.com>
2020-06-08 15:30:50 +0530

    ready for input

--- ex2.py
+++ ex2.py
@@ -8,12 +8,18 @@ a string with all the items separated by a comma and a space, with and
 inserted before the last item. For example, passing the previous spam list to
 the function would return 'apples, bananas, tofu, and cats'. But your function
 should be able to work with any list value passed to it. '''
+import sys
 def comma_code(parameter_list):
     string = str(parameter_list[0])
     for i in range(1,len(parameter_list)):
-        string += (", "+parameter_list[i])
+        string += (", "+str(parameter_list[i]))
     return string
 if __name__ == "__main__":
-    spam = ['apples', 'bananas', 'tofu', 'cats']
+    #spam = ['apples', 'bananas', 'tofu', 'cats']       #default values
+    try:
+        spam = list(input("List as a input to see output\n"))
+    except (NameError,ValueError,TypeError) as identifier:
+        print("Error occured try again with correct input")
+        sys.exit()
     out = comma_code(spam)
     print(out)
