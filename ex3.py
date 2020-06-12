1113fb1742794739b1c8930220d68580587fa168
DARSHAN P B <darshanpb111@gmail.com>
2020-06-12 15:49:22 +0530

    tic toc game

--- ex3.py
+++ ex3.py
@@ -38,5 +38,5 @@ if __name__ == "__main__":
     for i in range(len(grid[0])):
         for j in range(len(grid)):
             print(grid[j][i],end=' ')
-        print()
+        #print()
         
