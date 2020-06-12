5d909de0eaf77120487c6391e0841d5ab8d881ea
DARSHAN P B <darshanpb111@gmail.com>
2020-06-12 11:47:48 +0530

    ex3

---
+++ ex3.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+''' 
+Say you have a list of lists where each value in the inner lists is a one-character
+string, like this:
+grid = [['.', '.', '.', '.', '.', '.'],
+['.', 'O', 'O', '.', '.', '.'],
+['O', 'O', 'O', 'O', '.', '.'],
+['O', 'O', 'O', 'O', 'O', '.'],
+['.', 'O', 'O', 'O', 'O', 'O'],
+['O', 'O', 'O', 'O', 'O', '.'],
+['O', 'O', 'O', 'O', '.', '.'],
+['.', 'O', 'O', '.', '.', '.'],
+['.', '.', '.', '.', '.', '.']]
+You can think of grid[x][y] as being the character at the x- and
+y-coordinates
+of a “picture” drawn with text characters. The (0, 0) origin
+will be in the upper-left corner, the x-coordinates increase going right,
+and w the y-coordinates increase going down.
+Copy the previous grid value, and write code that uses it to print the image.
+..OO.OO..
+.OOOOOOO.
+.OOOOOOO.
+..OOOOO..
+...OOO...
+....O....
+ '''
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+if __name__ == "__main__":
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            print(grid[j][i],end=' ')
+        print()
+        
